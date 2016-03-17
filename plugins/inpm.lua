e481afc29de234bc0bdac5a588b45f60f7a20c9a
imashiam <ashkanpj.ap2@gmail.com>
2016-03-17 11:58:25 +0330

    Update inpm.lua

--- plugins/inpm.lua
+++ plugins/inpm.lua
@@ -208,7 +208,7 @@ if to == 'user' or service or is_admin1(msg) and to == "chat" or to == "channel"
 
 	if matches[1] == 'help' and msg.to.type == 'user' or matches[1] == 'pmhelp' and is_admin1(msg) and msg.to.type ~= 'user' then
       	savelog(msg.to.id, name_log.." ["..msg.from.id.."] used pm help")
-		text = "Welcome to A H D!\n\nTo get a list of A H D groups use /chats or /chatlist for a document list of chats.\n\nTo get a new A H D group, contact a support group:\n\nFor English support, use: /join English support\n\nFor Persian support, use: /join Persian support\n\nFor more information.\n\nThanks for using @THE_A_H_D!"
+		text = "Welcome to T E L E TG!\n\nTo get a list of TG groups use /chats or /chatlist for a document list of chats.\n\nTo get a new TG group, contact a support group:\n\nFor English support, use: /join English support\n\nFor Persian support, use: /join Persian support\n\nFor more information.\n\nThanks for using @TELE_TG!"
      	return text
     end
 
